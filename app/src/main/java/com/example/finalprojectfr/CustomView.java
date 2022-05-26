2d417e251f68b2fe0690fa296c0f4a7424db2f60
yumegan <yu.megan@gmail.com>
2022-05-26 14:08:31 -0400

    home page drawing

--- app/src/main/java/com/example/finalprojectfr/CustomView.java
+++ app/src/main/java/com/example/finalprojectfr/CustomView.java
@@ -4,29 +4,37 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomView  extends View{
+    private List<Point> points = new ArrayList<>();
+
     public CustomView(Context context, AttributeSet attr) {
         super(context);
     }
+
     @Override
     public void onDraw(Canvas canvas){
 
         canvas.drawColor(Color.rgb(32,32,32));
 
-        Paint redPaint = new Paint();
-        redPaint.setColor(Color.rgb(255, 0, 0));
+        for(Point p : points){
+            Paint paint = new Paint();
+            paint.setColor(Color.rgb((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256)));
+            canvas.drawCircle(p.x, p.y, 50, paint);
+        }
+    }
 
-        Paint greenPaint = new Paint();
-        greenPaint.setColor(Color.rgb(0, 255, 0));
-
-        Paint bluePaint = new Paint();
-        bluePaint.setColor(Color.rgb(0, 0, 255));
-
-        canvas.drawCircle(800, 500, 200, redPaint);
-        canvas.drawCircle(325, 900, 300, greenPaint);
-        canvas.drawCircle(900, 1600, 400, bluePaint);
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        points.add(new Point((int)event.getX(), (int)event.getY()));
+        invalidate();
+        return true;
     }
 }
